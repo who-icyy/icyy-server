46e72485918897901bd589e549a719589acf1eaf
who-icyy <itscobuu@gmail.com>
2024-10-10 19:25:29 +0530

    location

--- index.js
+++ index.js
@@ -1,38 +1,31 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const axios = require('axios');
 const app = express();
-const PORT = 3000;
 
-
-app.use(express.json());
-
-
-const mongoURI = 'mongodb+srv://<username>:<password>@cluster0.mongodb.net/my_database?retryWrites=true&w=majority';
-
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB Atlas'))
-    .catch((error) => console.error('Error connecting to MongoDB Atlas:', error));
-
-
-const dataSchema = new mongoose.Schema({
-    name: String,
-    age: Number,
-    email: String
-});
-
-const DataModel = mongoose.model('Data', dataSchema);
-
-// API
-app.post('/add-data', async (req, res) => {
+async function getLocation(ip) {
     try {
-        const newData = new DataModel(req.body);
-        await newData.save();
-        res.status(201).json({ message: 'Data saved successfully', data: newData });
+        const response = await axios.get(`http://ip-api.com/json/${ip}`);
+        console.log(response.data);
     } catch (error) {
-        res.status(500).json({ error: 'Error saving data' });
+        console.error('Error fetching location:', error);
     }
+}
+
+app.set("trust proxy", true);
+
+app.use((req, resp, next) => {
+    const ip = req.ip;
+    getLocation(ip);
+    
+})
+
+app.get('/', (req, res) => {
+    res.send('Hello, World! This is a simple web server using Express.js');
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+
+const PORT = 3000;
+const HOST = "Your IP";
+app.listen(PORT, HOST, () => {
+    console.log(`Server is running on http://${HOST}:${PORT}`);
 });
