53b1f46268eac15fe07c1643b20791218b31141b
who-icyy <itscobuu@gmail.com>
2024-10-11 08:25:02 +0530

    Final App

--- index.js
+++ index.js
@@ -1,33 +1,49 @@
 const express = require('express');
 const axios = require('axios');
+const geoip = require('geoip-lite')
 const app = express();
-
-async function getPublicIPLocation() {
-    try {
-      const ipResponse = await axios.get('https://api.ipify.org?format=json');
-
-      const publicIP = ipResponse.data.ip;
-      
-      const locationResponse = await axios.get(`https://ipinfo.io/${publicIP}/json?token=YOUR_TOKEN`);
-
-      console.table(locationResponse.data)
-
-      
-
-    } catch (error) {
-      console.error('Error:', error.message);
-    }
-  }
 
 app.set("trust proxy", true);
 
-app.get('/', (req, res) => {
-    getPublicIPLocation()
+async function getPublicIPLocation() {
+  try {
+    const ipResponse = await axios.get('https://api.ipify.org?format=json');
+
+    const publicIP = ipResponse.data.ip;
+
+
+    dns.reverse(publicIP, (err, hostnames) => {
+      if (err) {
+        console.error(`Error: ${err.message}`);
+      } else {
+        console.log(`Hostnames for IP ${publicIP}: ${hostnames.join(', ')}`);
+      }
+    });
+
+    // If you Do not want to use API then uncomment this Andcomment locationResponse
+
+    // const geo = geoip.lookup(publicIP) 
+
+    // console.table(geo)
+
+
+    const locationResponse = await axios.get(`https://ipinfo.io/${publicIP}/json?token=YOUR_TOKEN`);
+
+    console.table(locationResponse.data)
+
+  } catch (error) {
+    console.error('Error:', error.message);
+  }
+}
+
+
+app.get('/', () => {
+  getPublicIPLocation()
 });
 
 
 const PORT = 3000;
 const HOST = "YOUR IP ADDRESS";
 app.listen(PORT, HOST, () => {
-    console.log(`Server is running on http://${HOST}:${PORT}`);
+  console.log(`Server is running on http://${HOST}:${PORT}`);
 });
